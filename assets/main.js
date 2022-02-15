abbe107d48b7750343585060772dd87cda591ed9
NidhiSharma63 <nidhisharma639593@gmail.com>
2022-02-15 14:30:37 +0530

    winning function define

--- assets/main.js
+++ assets/main.js
@@ -3,23 +3,49 @@ let container = document.querySelector(".container");
 let items = document.querySelectorAll(".item");
 let declare = document.getElementById("declare")
 const space = [];
-let turn="x";
-// items.forEach(function (item) {
-//   item.addEventListener("click", function (e) {
-//   });
-// });
+let turn = "x";
+let gameover = false;
+
 // function to change the turn
 
-const changeTurn=()=>{
-  return turn === "x" ? "0": "x"
+const changeTurn = () => {
+  return turn === "x" ? "0" : "x"
+}
+
+// function to makeWinner
+const winnerFunction = (board) => {
+  if ((container.children[0].innerText === "x") && (container.children[1].innerText === "x") && (container.children[2].innerText === "x")
+  ||(container.children[0].innerText === "x") && (container.children[3].innerText === "x") && (container.children[6].innerText === "x")
+  ||(container.children[0].innerText === "x") && (container.children[4].innerText === "x") && (container.children[8].innerText === "x")
+  ) {
+    gameover=true
+    declare.innerText=`you win`
+    // return true;
+  }
+
+  if ((container.children[4].innerText === "x") && (container.children[3].innerText === "x") && (container.children[5].innerText === "x")
+  ||(container.children[4].innerText === "x") && (container.children[1].innerText === "x") && (container.children[7].innerText === "x")
+  ||(container.children[4].innerText === "x") && (container.children[2].innerText === "x") && (container.children[6].innerText === "x")
+  ) {
+    gameover=true
+    declare.innerText=`you win`
+    // return true;
+  }
+  if ((container.children[8].innerText === "x") && (container.children[6].innerText === "x") && (container.children[7].innerText === "x")
+  ||(container.children[8].innerText === "x") && (container.children[5].innerText === "x") && (container.children[2].innerText === "x")
+  ) {
+    gameover=true
+    declare.innerText=`you win`
+  }
+
 }
 items.forEach(function (item) {
   item.addEventListener("click", function (e) {
-    if(e.target.innerText===''){
-      e.target.innerText=turn;
-    turn = changeTurn();
-
-  console.log(e.target.innerText)
+    let currentTarget = e.target;
+    if (currentTarget.innerText === '') {
+      currentTarget.innerText = turn;
+      turn = changeTurn();
+      winnerFunction(); 
     }
   });
 });
