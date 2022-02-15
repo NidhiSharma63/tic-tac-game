e8b3fd3177eb962faf27cc1ba6a4fc7eff31dce5
NidhiSharma63 <nidhisharma639593@gmail.com>
2022-02-15 11:48:26 +0530

    current player define

---
+++ assets/explaination.md
@@ -0,0 +1,2 @@
+1.why define space
+==>so that each time when user click the item box every time data-set-id came in the space variable and every time it check that id is not repeated if id is repeated then it will do nothing else it will place zero 
--- assets/main.js
+++ assets/main.js
@@ -1,66 +1,19 @@
-var container = document.querySelector(".container");
-var items = document.querySelectorAll(".item");
+
+
+let container = document.querySelector(".container");
+let items = document.querySelectorAll(".item");
+const space = [];
+const tick_zero = "0";
+const tick_cross = "x";
+let current_player = tick_zero;
 items.forEach(function (item) {
-  if (item) {
-    item.addEventListener("click", function () {
-    //   if(winnner){
-    //       alert("your are winner")
-    //   } 
-      var firstChildNode = item.childNodes[1];
-      var secondChildNode = item.childNodes[3];
-      if (!secondChildNode.classList.contains("show")) {
-        firstChildNode.classList.add("show");
-      } else {
-      }
-      items.forEach(function (lastItem) {
-        if (lastItem == item) {
-        }
-      });
-      var randomId = Math.floor(Math.random() * 10);
-      console.log(randomId);
-      var firstChild = container.children[randomId].firstElementChild;
-      var lastChild = container.children[randomId].lastElementChild;
-
-      setTimeout(() => {
-        if (
-          firstChild.classList.contains("show") ||
-          lastChild.classList.contains("show")
-        ) {
-        } else {
-          lastChild.classList.add("show");
-        }
-      }, 20);
-    
-    });
-    
+ item.addEventListener("click",function(e){
+  let id = e.target.dataset.id;
+  if(!space[id]){
+    e.target.innerHTML=current_player
   }
-});
+ })
+  
 
-function winnner() {
-  return (
-    (container.children[0].firstElementChild.classList.contains("show") &&
-      container.children[1].firstElementChild.classList.contains("show") &&
-      container.children[2].firstElementChild.classList.contains("show")) ||
-    (container.children[3].firstElementChild.classList.contains("show") &&
-      container.children[4].firstElementChild.classList.contains("show") &&
-      container.children[5].firstElementChild.classList.contains("show")) ||
-    (container.children[6].firstElementChild.classList.contains("show") &&
-      container.children[7].firstElementChild.classList.contains("show") &&
-      container.children[8].firstElementChild.classList.contains("show")) ||
-    (container.children[0].firstElementChild.classList.contains("show") &&
-      container.children[4].firstElementChild.classList.contains("show") &&
-      container.children[8].firstElementChild.classList.contains("show")) ||
-    (container.children[2].firstElementChild.classList.contains("show") &&
-      container.children[4].firstElementChild.classList.contains("show") &&
-      container.children[6].firstElementChild.classList.contains("show")) ||
-    (container.children[0].firstElementChild.classList.contains("show") &&
-      container.children[3].firstElementChild.classList.contains("show") &&
-      container.children[6].firstElementChild.classList.contains("show")) ||
-    (container.children[1].firstElementChild.classList.contains("show") &&
-      container.children[4].firstElementChild.classList.contains("show") &&
-      container.children[7].firstElementChild.classList.contains("show")) ||
-    (container.children[2].firstElementChild.classList.contains("show") &&
-      container.children[5].firstElementChild.classList.contains("show") &&
-      container.children[8].firstElementChild.classList.contains("show"))
-  );
-}
+})
+// make the border using js
